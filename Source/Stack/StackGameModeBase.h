45c5ab94ebc34dfc423f335198b37012bb54e7ff
Ashot Barkhudaryan <ashot7410@gmail.com>
2018-04-17 00:26:39 +0400

    Init

---
+++ Source/Stack/StackGameModeBase.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StackGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STACK_API AStackGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	AStackGameModeBase();
+	
+	
+	
+};
